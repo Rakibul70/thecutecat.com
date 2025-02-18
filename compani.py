002a27fcb27ebc5d231dec6e2c2a79b74a54d4a8
Rakibul70 <rakibulhasan.khsc@gmail.com>
2025-02-18 19:57:45 +0600

    Update compani.py

--- compani.py
+++ compani.py
@@ -1,1 +1,25 @@
+import sys
+
+
+fj = "4j8i288"
+print("WELCOME TO OUR COMPANIC PAGE")
+print("DO YOU WANT TO ENTER?")
+
+inputt = input("Enter The Company Password: ")
+if inputt == fj:
+    print("Welcome to our Company")
+else:
+    print("Incorrect password. Access denied.")
+    sys.exit()
+
+print("ENTER YOUR EMAIL ADDRESS")
+aluu = input("Email :")
+fl = "khsc@ridoy183"
+
+print("ENTER YOUR EMAIL PASSWORD")
+iii = input("Password:")
+if iii == fl:
+    print("VERIFIED! WELCOME")
+else:
+    print("Incorrect password. Access denied.")
 
